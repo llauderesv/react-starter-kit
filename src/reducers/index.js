1159abf07374507a46a5644b3af38496bb338682
llauderesv <llauderesv@gmail.com>
2019-04-15 09:52:20 +0800

    add comment in reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { connectRouter } from 'connected-react-router';
 import { combineReducers } from 'redux';
+
+// Import all app reducers...
 import * as counter from './counter';
 import * as error from './error';
 
+// Add all your app reducers here
+// In combine reducers
 const rootReducers = history =>
   combineReducers({
     counter: counter.reducer,
